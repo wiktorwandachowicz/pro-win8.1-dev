e93d354c34b7b063e821203a3a734339255dcb64
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2014-12-15 16:06:39 +0100

    GridView1: Simple add and insert items into GridView control

---
+++ Chap05/GridView1/GridView1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GridView1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private uint counter = 1;
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            myGridView.Items.Add("Item " + counter++);
+        }
+
+        private void Insert_Click(object sender, RoutedEventArgs e)
+        {
+            myGridView.Items.Insert(0, "Item " + counter++);
+        }
+    }
+}
