7ee1388338b5d021e1219f8f602ad90105ce1ba6
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2014-12-15 16:24:07 +0100

    GridView2: Display persons in groups using initially sorted Collections of Collections

---
+++ Chap05/GridView2/GridView2/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using GridView2.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GridView2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private IList<Person> people;
+        private IList<Group<object, Person>> groupedPeople;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            people = Person.CreatePeople(200).ToList();
+            groupedPeople = (from person in people
+                             group person by person.City into g
+                             orderby g.Key
+                             select new Group<object, Person>
+                             {
+                                Key = g.Key.ToString(),
+                                Items = g.ToList()
+                             }).ToList();
+            cvs.Source = groupedPeople;
+        }
+    }
+}
