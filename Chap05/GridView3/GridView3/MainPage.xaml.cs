3e45c294f3c21de4f0b9ef653c4818acc4351d08
Wiktor Wandachowicz <wiktor.wandachowicz@p.lodz.pl>
2014-12-21 13:30:52 +0100

    GridView3: Manual switching between GridViev and ListView controls

---
+++ Chap05/GridView3/GridView3/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using GridView3.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GridView3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private IList<Person> people;
+        private ObservableCollection<Group<string, Person>> groupedPeople;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            people = Person.CreatePeople(25).ToList();
+            FilterPeople();
+        }
+
+        private void FilterPeople()
+        {
+            var result = (from person in people
+                          group person by person.City into g
+                          orderby g.Key
+                          select new Group<string, Person>
+                          {
+                              Key = g.Key.ToString(),
+                              Items = new ObservableCollection<Person>(g.ToList())
+                          }).ToList();
+            groupedPeople = new ObservableCollection<Group<string, Person>>(result);
+            cvs.Source = groupedPeople;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<Person> people = Person.CreatePeople(1);
+            Person person = people[0];
+            var result = (from g in groupedPeople
+                          where g.Key == person.City
+                          select g);
+            if (result.Count() > 0)
+            {
+                Group<string, Person> group = result.First();
+                //group.Items.Add(person);
+                group.Items.AddSorted(person);
+            }
+            else
+            {
+                Group<string, Person> group = new Group<string, Person>
+                {
+                    Key = person.City,
+                    Items = new ObservableCollection<Person>(people.ToList())
+                };
+                //groupedPeople.Add(group);
+                groupedPeople.AddSorted(group);
+            }
+            cvs.Source = groupedPeople;
+        }
+
+        private void CmdSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            myGridView.Visibility = (myGridView.Visibility == Visibility.Collapsed) ?
+                Visibility.Visible : Visibility.Collapsed;
+            myListView.Visibility = (myListView.Visibility == Visibility.Collapsed) ?
+                Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
---
+++ Chap05/GridView3/GridView3/Model/Person.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridView3.Model
+{
+    public class Person : INotifyPropertyChanged, IComparable<Person>
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string city;
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                city = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} ({2})", FirstName, LastName, City);
+        }
+
+        public int CompareTo(Person other)
+        {
+            string s1 = String.Format("{0} {1}", this.FirstName, this.LastName);
+            string s2 = String.Format("{0} {1}", other.FirstName, other.LastName);
+            return s1.CompareTo(s2);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string caller = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+
+        private static readonly string[] firstNames = { "Adam", "Bob", "Carl", "David", "Edgar", "Frank",
+                                                          "George", "Harry", "Isaac", "Jesse", "Ken", "Larry" };
+        private static readonly string[] lastNames = { "Aaronson", "Bobson", "Carlson", "Davidson", "Enstwhile", "Ferguson",
+                                                         "Goodyear", "Harrison", "Isaacson", "Jackson", "Kennelworth", "Levine" };
+        private static readonly string[] cities = { "Boston", "New York", "LA", "San Francisco", "Phoenix",
+                                                      "San Jose", "Cincinnati", "Bellevue" };
+        public static ObservableCollection<Person> CreatePeople(int count)
+        {
+            var people = new ObservableCollection<Person>();
+            var r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                var p = new Person()
+                {
+                    FirstName = firstNames[r.Next(firstNames.Length)],
+                    LastName = lastNames[r.Next(lastNames.Length)],
+                    City = cities[r.Next(cities.Length)]
+                };
+                people.AddSorted(p);
+            }
+            return people;
+        }
+    }
+}
